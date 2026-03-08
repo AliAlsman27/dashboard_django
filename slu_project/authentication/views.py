3667d304de4c15ce71e21ce60ae6f53b10b07193
alialsman27 <al.elsmman27@gmail.com>
2026-03-09 01:06:52 +0200

    add auth app temp

---
+++ slu_project/authentication/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .forms import userform
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = userform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = userform()
+    return render(request, 'register.html', {'form': form})
